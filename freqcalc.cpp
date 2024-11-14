3c3fad710263c407a11e87f1abe0cdbc98169c24
kashregister24 <58663785+kashregister@users.noreply.github.com>
2024-11-14 20:22:18 +0100

    Update freqcalc.cpp
    added support for command line arguments

--- freqcalc.cpp
+++ freqcalc.cpp
@@ -4,18 +4,24 @@
 #include <cmath>
 
 double returnRatio(int interval) {
-    return pow(2,(double(interval) / 12));
+    return pow(2, (double(interval) / 12));
 }
 
-int main()
+int main(int argc, char** argv)
 {
     std::array<std::string, 12> twelveInterval = { "C" ,"C#" ,"D" ,"D#", "E","F" ,"F#" ,"G" ,"G#", "A", "A#","B" };
     int octave = 4;
     double a_freq;
-    std::cout << "Enter the A4 freq (standard is 440)" << std::endl;
-    std::cin >> a_freq;
+
+    if (argv[1] != NULL) {
+        a_freq = atof(argv[1]);
+    } else {
+        std::cout << "Enter the A4 freq (standard is 440)" << std::endl;
+        std::cin >> a_freq;
+    }
+
     double c_freq = a_freq / 1.6817900361584857774074442142601;
-    
+
     for (int n = 0; n < 5; n++) {
         for (int i = 0; i < twelveInterval.size(); i++) {
             double ratio = returnRatio(i);
@@ -23,7 +29,7 @@ int main()
             std::cout << twelveInterval[i] << std::to_string(octave) << " = " << std::to_string(frequency);
             std::cout << "    ratio: " << i << "/12   =   ";
             printf("%.3f", ratio);
-            std::cout<< std::endl;
+            std::cout << std::endl;
         }
         c_freq *= 2;
         std::cout << std::endl;
