a7c5082879478f86ff9b351465d897223b3d7940
kashregister24 <58663785+kashregister@users.noreply.github.com>
2024-11-14 18:42:42 +0100

    Update freqcalc.cpp
    input for reference tune (a4)

--- freqcalc.cpp
+++ freqcalc.cpp
@@ -11,9 +11,12 @@ int main()
 {
     std::array<std::string, 12> twelveInterval = { "C" ,"C#" ,"D" ,"D#", "E","F" ,"F#" ,"G" ,"G#", "A", "A#","B" };
     int octave = 4;
-    double c_freq = 261.626;
-
-    for (int n = 0; n < 3; n++) {
+    double a_freq;
+    std::cout << "Enter the A4 freq (standard is 440)" << std::endl;
+    std::cin >> a_freq;
+    double c_freq = a_freq / 1.6817900361584857774074442142601;
+    
+    for (int n = 0; n < 5; n++) {
         for (int i = 0; i < twelveInterval.size(); i++) {
             double ratio = returnRatio(i);
             double frequency = c_freq * ratio;
